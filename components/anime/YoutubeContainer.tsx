6f905782b823a1de3089abfab219232954ca8e5d
KAL <kevin12147@yahoo.com.tw>
2025-02-20 15:56:04 +0800

    replaced Link with useRouter

--- components/anime/YoutubeContainer.tsx
+++ components/anime/YoutubeContainer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import YouTube from "react-youtube";
 
 export default function YoutubeContainer({
--- components/main/Pagination.tsx
+++ components/main/Pagination.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect } from "react";
-import Link from "next/link";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export default function Pagination({
@@ -32,7 +31,7 @@ export default function Pagination({
       const findIndex = totalYears.indexOf(checkYear);
       params.set("year", totalYears[findIndex + 1]);
     }
-    return `${pathname}?${params.toString()}`;
+    router.push(`${pathname}?${params.toString()}`);
   };
 
   const handleNextYear = () => {
@@ -41,21 +40,28 @@ export default function Pagination({
       const findIndex = totalYears.indexOf(checkYear);
       params.set("year", totalYears[findIndex - 1]);
     }
-    return `${pathname}?${params.toString()}`;
+    router.push(`${pathname}?${params.toString()}`);
   };
-
-  const prevYearUrl = handlePrevYear();
-  const nextYearUrl = handleNextYear();
 
   return (
     <>
       <div className="flex justify-center items-center gap-10 text-gray-200 pb-5 pt-2">
         {totalYears[totalYears.length - 1] === currentYear ||
         query !== "" ? null : (
-          <Link href={prevYearUrl}>上一年</Link>
+          <div
+            onClick={handlePrevYear}
+            className="cursor-pointer hover:text-blue-600"
+          >
+            上一年
+          </div>
         )}
         {totalYears[0] === currentYear || query !== "" ? null : (
-          <Link href={nextYearUrl}>下一年</Link>
+          <div
+            onClick={handleNextYear}
+            className="cursor-pointer hover:text-blue-600"
+          >
+            下一年
+          </div>
         )}
       </div>
     </>
